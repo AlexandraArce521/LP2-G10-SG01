3bab4f13879e106c8b0f045383aa18aeb4b26d60
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-05-07 16:47:06 -0500

    Update Ejercicio_1.cpp

--- Lab N°7/Ejercicios/Ejercicio_1.cpp
+++ Lab N°7/Ejercicios/Ejercicio_1.cpp
@@ -1,1 +1,26 @@
-a
+#include <iostream>
+using namespace std;
+
+class Vehiculo {
+    string marca;
+    string modelo;
+    int anyoFabricacion;
+};
+
+class Automovil: public Vehiculo {
+    int numPuertas:
+    string tipoCombustible;
+    
+};
+
+class Automovil: public Motocicleta {
+    string tipoCombustible;
+    
+};
+
+
+int main()
+{
+
+    return 0;
+}
