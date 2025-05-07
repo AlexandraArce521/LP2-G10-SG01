80cd1e975cf712b4c120785760bca4131879e4f9
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-05-07 16:51:00 -0500

    Update Ejercicio_2.cpp

--- Lab N°7/Ejercicios/Ejercicio_2.cpp
+++ Lab N°7/Ejercicios/Ejercicio_2.cpp
@@ -1,1 +1,35 @@
-a
+
+#include <iostream>
+using namespace std;
+
+class Persona {
+    string nombre;
+    int edad;
+    double salario;
+};
+
+class Correo : protected Vehiculo {
+    int numPuertas:
+    string tipoCombustible;
+    
+};
+
+class Automovil: public Motocicleta {
+    string correoElectronico;
+    
+};
+
+class Empleado: public {
+    
+};
+
+class Cliente: public {
+    
+};
+
+
+int main()
+{
+
+    return 0;
+}
