e5185b5d24c8f2fc03de9ae645884f21bf04c816
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-05-07 17:00:18 -0500

    Update Ejercicio_1.cpp

--- Lab N°7/Ejercicios/Ejercicio_1.cpp
+++ Lab N°7/Ejercicios/Ejercicio_1.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 class Vehiculo {
+protected:
     string marca;
     string modelo;
     int anyoFabricacion;
+public: 
+Vehiculo() {
+    marca = "Sin marca";
+    modelo = "Sin modelo";
+    anyoFabricacion = 0;
+}
 };
 
 class Automovil: public Vehiculo {
