03c2eb84040426fac5b8ec6f14496b92ae984fca
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-05-09 15:50:26 -0500

    Update Ejercicio_3.cpp

--- Lab N°7/Ejercicios/Ejercicio_3.cpp
+++ Lab N°7/Ejercicios/Ejercicio_3.cpp
@@ -1,1 +1,70 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
+class Movil {
+protected:
+    string marca;
+    string modelo;
+    string SO;
+
+public:
+    Movil(string m, string model, string so)
+     : marca(m), modelo(model), SO(so) {
+        cout << "Constructor de la clase Movil invocado" << endl;
+    }
+    
+    void setMarca(string m) {
+        marca = m;
+    }
+    
+    void setModelo(string model) {
+        modelo = model;
+    }
+    
+    void setSO (string so) {
+        SO = so;
+    }
+    
+    ~Movil() {
+        cout << "Destructor invocado" << endl;
+    }
+    
+
+};
+
+class Telefono: protected Movil {
+private:
+    vector <string> aplicaciones;
+    string gama;
+
+
+public:
+    Telefono(string m, string model, string so, vector<string>a, string g)
+     : Movil(m, model, so), aplicaciones(a), gama(g)
+    {
+        cout << "Constructor con parametros de " << marca << endl;
+    }
+
+    ~Telefono() {
+        cout << "Destructor de Telefono con número: " << marca << endl;
+    }
+
+};
+
+class Tablet: protected Movil {
+private:
+    bool tieneLapiz;
+
+public:
+
+    
+};
+
+int main()
+{
+
+    
+    return 0;
+}
