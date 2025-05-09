2e34b7e745f6178222711faff1fcc9e6ae746194
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-05-09 15:11:41 -0500

    Update Ejercicio_1.cpp

--- Lab N°7/Ejercicios/Ejercicio_1.cpp
+++ Lab N°7/Ejercicios/Ejercicio_1.cpp
@@ -1,33 +1,151 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Vehiculo {
 protected:
-    string marca;
-    string modelo;
-    int anyoFabricacion;
-public: 
-Vehiculo() {
-    marca = "Sin marca";
-    modelo = "Sin modelo";
-    anyoFabricacion = 0;
-}
+    string marca, modelo, anyo;
+    double precio;
+
+public:
+    
+    Vehiculo(string marca, string modelo, string anyo, double precio) {
+        this->marca = marca;
+        this->modelo = modelo;
+        this->anyo = anyo;
+        this->precio = precio;
+        cout << "Constructor de clase base Vehiculo" << endl;
+    }
+    
+    string getMarca() {
+        return marca;
+    }
+    
+    string getModelo() {
+        return modelo;
+    }
+    
+    string getAnyo() {
+        return anyo;
+    }
+    
+    double getPrecio() {
+        return precio;
+    }
+    
+    void setMarca(string m) {
+        marca = m;
+    }
+    
+    void setModelo(string mo) {
+        modelo = mo;
+    }
+    
+    void setAnyo(string a) {
+        anyo = a;
+    }
+    
+    void setPrecio(double p) {
+        precio = p;
+    }
+    
+    ~Vehiculo() {
+        cout << "Destructor de clase base Vehiculo" << endl;
+    }
+        
 };
 
 class Automovil: public Vehiculo {
-    int numPuertas:
+private:
+    int numPuertas;
     string tipoCombustible;
+
+public:
+    Automovil(string marca, string modelo, string anyo, double precio, int num, string tipo)
+        : Vehiculo(marca, modelo, anyo, precio), numPuertas(num), tipoCombustible(tipo) {
+        cout << "Constructor de clase derivada Automovil" << endl;
+    }
+
+    void mostrarAuto() {
+        cout << "==================================" << endl;
+        cout << "Detalles del Automovil.\n" << endl;
+        cout << "Marca: " << marca << endl;
+        cout << "Modelo: " << modelo << endl;
+        cout << "Año: " << anyo << endl;
+        cout << "Precio: " << precio << endl;
+        cout << "Número de Puertas: " << numPuertas << endl;
+        cout << "Tipo de combustible: " << tipoCombustible << endl;
+        cout << "\n==================================" << endl;
+
+    }
     
+    int getNumPuertas() {
+        return numPuertas;
+    }
+    
+    string getTipo() {
+        return tipoCombustible;
+    }
+    
+    void setNumPuertas(int p) {
+        numPuertas = p;
+    }
+    
+    void setTipo(string tipo) {
+        tipoCombustible = tipo;
+    }
+
+    ~Automovil () {
+        cout << "Destructor de clase derivada Automovil" << endl;
+    }
+    
+
+
 };
 
-class Automovil: public Motocicleta {
-    string tipoCombustible;
+class Motocicleta: public Vehiculo {
+private:
+    int cilindrada;
     
-};
+public:
+    Motocicleta(string marca, string modelo, string anyo, double precio, int cc)
+        : Vehiculo(marca, modelo, anyo, precio), cilindrada(cc) {
+        cout << "Constructor de clase derivada Motocicleta" << endl;
+    }
+    
+    int getCilindrica() {
+        return cilindrada;
+    }
+    
+    void setCilindrica(int cc) {
+        cilindrada = cc;
+    }
+    
+    void mostrarMoto() {
+        cout << "==================================\n" << endl;
+        cout << "Detalles de Motocicleta." << endl;
+        cout << "Marca: " << marca << endl;
+        cout << "Modelo: " << modelo << endl;
+        cout << "Año: " << anyo << endl;
+        cout << "Precio: " << precio << endl;
+        cout << "Cilindrada: " << cilindrada << " cc" <<endl;
+        cout << "\n==================================" << endl;
 
+    }
+    
+    ~Motocicleta() {
+       cout << "Destructor de clase derivada Motocicleta" << endl; 
+    }
+};
 
 int main()
 {
+    Automovil auto1("Toyota", "Corolla", "2023", 25000.0, 4, "Gasolina");
+    auto1.mostrarAuto();
+    cout << endl;
+    Motocicleta moto1("Honda", "CBR500R", "2022", 7000.0, 8);
+    moto1.mostrarMoto();
+    
 
     return 0;
 }
