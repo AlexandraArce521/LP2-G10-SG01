32b6459cef8eb2bafc3f608ec0bea0556dbffe80
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-05-18 16:54:04 -0500

    Update Ejercicio_1.cpp

--- Lab N°8/Ejercicios/Ejercicio_1.cpp
+++ Lab N°8/Ejercicios/Ejercicio_1.cpp
@@ -1,1 +1,90 @@
-xd
+
+#include <iostream>
+using namespace std;
+
+class Figura {
+public:
+    virtual void dibujar () = 0;
+    virtual double area () = 0;
+};
+
+class Circulo: public Figura {
+private:
+    double radio;
+    const double pi = 3.14;
+    
+public:
+    Circulo(double r)
+        :radio(r) {}
+    
+    void dibujar() override {
+        cout << "Estoy dibujando un Círculo\n";
+    }
+    
+    double area() override {
+        return pi*radio*radio;
+    }
+};
+
+class Rectangulo: public Figura {
+private:
+    double base;
+    double altura;
+
+public:
+    Rectangulo(double b, double a)
+    : base(b), altura(a) {}
+    
+    void dibujar() override {
+        cout << "Estoy dibujando un Rectangulo\n";
+    }
+    
+    double area() override {
+        return base*altura;
+    }
+};
+
+class Triangulo: public Figura {
+private:
+    double base;
+    double altura;    
+public:
+    Triangulo(double b, double a)
+    : base(b), altura(a) {}
+    
+    void dibujar() override {
+        cout << "Estoy dibujando un Triángulo\n";
+    }
+    
+    double area() override {
+        return (base*altura)/2;
+    }
+};
+
+int main()
+{
+    Figura* cuadrado1 = new Circulo(2);
+    Figura* rectangulo1 = new Rectangulo(5,2);    
+    Figura* triangulo1 = new Triangulo(3,4);
+
+    
+    cuadrado1 ->dibujar();
+    cout << "Área del cuadrado: " << cuadrado1 ->area() << endl;
+    cout << endl;
+    
+    triangulo1 ->dibujar();
+    cout << "Área del triangulo: " << triangulo1 ->area() << endl;
+    cout << endl;
+
+    
+    rectangulo1 ->dibujar();
+    cout<< "Área del rectángulo: " << rectangulo1 ->area() << endl;    
+    cout << endl;
+
+    
+    delete cuadrado1;
+    delete rectangulo1;
+    delete triangulo1;
+
+    return 0;
+}
