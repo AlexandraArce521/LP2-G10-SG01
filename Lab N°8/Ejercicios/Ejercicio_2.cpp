054d946cc20ecb4c7b3f3a36a3bff4e896c57246
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-05-18 21:14:04 -0500

    Update Ejercicio_2.cpp

--- Lab N°8/Ejercicios/Ejercicio_2.cpp
+++ Lab N°8/Ejercicios/Ejercicio_2.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+using namespace std;
 
+class Reproductor {
+public:
+    virtual void reproducir() = 0;
+    virtual void pausar() = 0;
+    virtual void detener() = 0;
+};
+
+class ReproductorMP3: public Reproductor {
+private:
+    string cancion;
+    
+public:
+    ReproductorMP3(string c)
+     :cancion(c) {}
+     
+    void reproducir() override {
+        cout << "Reproducir la canción: " << cancion << ".MP3" <<endl;
+    }
+    
+    void pausar() override {
+        cout << "Pausar la cancion: " << cancion << ".MP3" << endl;
+    }
+    
+    void detener() override {
+        cout << "Detener el ReproductorMP3\n";
+    }    
+    
+};
+
+int main()
+{
+    Reproductor* rp1 = new ReproductorMP3("Atlantis");
+    
+    rp1->reproducir();
+    rp1->pausar();
+    rp1->detener();    
+
+    delete rp1;
+    return 0;
+}
