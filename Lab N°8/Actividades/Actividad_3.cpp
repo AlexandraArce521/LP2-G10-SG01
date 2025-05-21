07487c8e734375eb40aa8ec27e25583ec27d3136
Kendy-16 <kelly.ramos.1652@gmail.com>
2025-05-20 19:31:42 -0500

    Update Actividad_3.cpp

--- Lab N°8/Actividades/Actividad_3.cpp
+++ Lab N°8/Actividades/Actividad_3.cpp
@@ -1,1 +1,56 @@
+#include <iostream>
+using namespace std;
 
+class Animal {
+public:
+    virtual void hacerSonido() const {
+        cout << "(sonido genérico)\n";
+    }
+    virtual ~Animal() {}
+};
+
+class Perro : public Animal {
+public:
+    void hacerSonido() const override {
+        cout << "Guau guau!\n";
+    }
+};
+
+class Gato : public Animal {
+public:
+    void hacerSonido() const override {
+        cout << "Miau miau!\n";
+    }
+};
+
+class Vaca : public Animal {
+public:
+    void hacerSonido() const override {
+        cout << "Muuu muuu!\n";
+    }
+};
+
+int main() {
+    cout << "===========================\n";
+    cout << "    SONIDOS DE ANIMALES    \n";
+    cout << "===========================\n";
+
+    Animal* a1 = new Perro();
+    Animal* a2 = new Gato();
+    Animal* a3 = new Vaca();
+
+    cout << "\nPerro dice: ";
+    a1->hacerSonido();
+
+    cout << "Gato dice: ";
+    a2->hacerSonido();
+
+    cout << "Vaca dice: ";
+    a3->hacerSonido();
+
+    delete a1;
+    delete a2;
+    delete a3;
+
+    return 0;
+}
