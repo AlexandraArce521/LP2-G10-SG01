308b6c5b854e1c62c7a0bb8d49b7214e1fbd1c0f
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-06-24 16:06:43 -0500

    Update Ejercicio_2.cpp

--- Lab N° 11/Ejercicios/Ejercicio_2.cpp
+++ Lab N° 11/Ejercicios/Ejercicio_2.cpp
@@ -1,1 +1,55 @@
-dsa
+/* 
+Crea una función en C++ llamada "divide" que acepte dos números enteros  
+como parámetros. La función debe dividir el primer número por el  
+segundo número y devolver el resultado.  
+Sin embargo, si el segundo número es igual a cero, la función debe lanzar 
+una excepción de tipo "std::runtime_error" con un mensaje  
+indicando que no se puede dividir por cero. Luego,  
+en el programa principal, llama a la función "divide" con 
+diferentes valores y maneja las excepciones adecuadamente. 
+*/ 
+  
+#include <fstream>  
+#include <iostream>  
+#include <stdexcept>  
+using namespace std;  
+  
+template <class T> 
+void divide(T a, T b) { 
+  
+    if(b == 0) { 
+        throw runtime_error("!"); 
+    }     
+    cout << "Dividir " << a << " / "  << b << " = "; 
+    cout << a/b; 
+    cout << "\n----------------------\n"; 
+} 
+  
+int main() { 
+     
+    try 
+    {     
+     
+    divide(10.0,4.0);  
+    divide(5,2);  
+    divide(47600L,2462L);  
+    divide(10.0,0.0); 
+  
+  
+    } 
+     
+    catch (runtime_error other) 
+    { 
+        cout << "(!) Error por dividir por cero" << endl; 
+    } 
+     
+    catch(invalid_argument other) { 
+        cout << "(!)  Error: Argementos no validos" << endl; 
+    } 
+    catch (...)  
+    { 
+        cout << "(!)Error inesperado" << endl; 
+    } 
+         
+    return 0; 
+} 
