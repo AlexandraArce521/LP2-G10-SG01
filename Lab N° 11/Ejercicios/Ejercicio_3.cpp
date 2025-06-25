bf621c9d631f01bce025e941609f02a6235db08d
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-06-24 20:15:12 -0500

    Update Ejercicio_3.cpp

--- Lab N° 11/Ejercicios/Ejercicio_3.cpp
+++ Lab N° 11/Ejercicios/Ejercicio_3.cpp
@@ -47,29 +47,79 @@ public:
             cout << "(!)Sin nombre. Ingrese un nombre\n";
             exit(1);
         }
+        
+    }
+    void mostrar() const {
+        cout << "Nombre: " << nombre << endl;
+        cout << "Edad: " << edad << endl;
+        cout << "Direccion: " << direccion << endl;
+    }
+    
+    void escribirArchivo(ofstream& archivo) {
+        size_t lenNombre = nombre.size();
+        size_t lenDireccion = direccion.size();
+    
+        archivo.write((char*)&lenNombre, sizeof(lenNombre));
+        archivo.write((char*)nombre.c_str(), lenNombre);
+    
+        archivo.write((char*)&edad, sizeof(edad));
+    
+        archivo.write((char*)&lenDireccion, sizeof(lenDireccion));
+        archivo.write((char*)direccion.c_str(), lenDireccion);
+    }
+
+    void leerArchivo(ifstream& archivo)  {
+        size_t lenNombre, lenDireccion;
+    
+        archivo.read((char*)&lenNombre, sizeof(lenNombre));
+        nombre.resize(lenNombre);
+        archivo.read(&nombre[0], lenNombre);
+    
+        archivo.read((char*)&edad, sizeof(edad));
+    
+        archivo.read((char*)&lenDireccion, sizeof(lenDireccion));
+        direccion.resize(lenDireccion);
+        archivo.read(&direccion[0], lenDireccion);
     }
 };
 
 int main()
 {   
-    vector <Persona*> personas;
+    // Declarar vector de personas
+    vector<Persona> personas;
+
+    personas.push_back(Persona("Juan", 2, "Av. Villa Hermosa"));
+    personas.push_back(Persona("Matias", 12, "Av. Arequipa"));
+    personas.push_back(Persona("Marta", 20, "Urb Misti"));
+    personas.push_back(Persona("Oscar", 45, "Urb. Rivera"));
+    personas.push_back(Persona("María", 70, "Av. Lima"));
+
+    // Escribir en archivo binario
+    ofstream archivoOut("Registro.bin", ios::out | ios::binary);
+    for (auto& p : personas)
+        p.escribirArchivo(archivoOut);
+    archivoOut.close();
+
+    //Leer lo que esta en el Archivo
+    vector<Persona> personasLeidas;
+    ifstream archivoIn("Registro.bin", ios::in |ios::binary);
     
-    personas.push_back(new Persona("Juan", 2, "Av. Villa Hermosa"));
-    personas.push_back(new Persona("Matias", 12, "Av. Arequipa"));
-    personas.push_back(new Persona("Marta", 20, "Urb Misti"));
-    personas.push_back(new Persona("Oscar", 45, "Urb. Rivera"));
-    personas.push_back(new Persona("María", 70, "Av. Lima"));
-
+    // Nuevo vector
+    for (int i = 0; i < 5; ++i) {
+        Persona p = Persona("", 0, "");
+        personasLeidas.push_back(p);
+    }
     
 
-    fstream archivo;
-    archivo.open("Registro.txt", ios::out);
+    // Mostrar las personas leídas
+    for (auto& p : personasLeidas) {
+        p.leerArchivo(archivoIn);
+        p.mostrar();
+        
+        cout << "------------------\n";
+    }
+    archivoIn.close();
 
-     for (int i = 0; i < personas.size(); ++i) {
-        archivo << personas[i] << endl;
-    }   
     
-    archivo.close();
-
     return 0;
 }
