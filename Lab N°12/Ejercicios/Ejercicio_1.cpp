e87e3f90e5227b3e6fa7a33aa9f27a2f15e69557
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-06-26 16:27:14 -0500

    Update Ejercicio_1.cpp

--- Lab N°12/Ejercicios/Ejercicio_1.cpp
+++ Lab N°12/Ejercicios/Ejercicio_1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+struct Inventario {
+    string codigo;
+    string descripcion;
+    int cantidad;
+};
+
+int main()
+{
+    vector <string> menu = {"____________________________________________________",
+                            "1. Agregar un nuevo producto", 
+                            "2. Actualizar la cantidad disponible",
+                            "3. Buscar un producto por su codigo",
+                            "4. Mostrar la lista de productos ordenados \n  alfabeticamente por su descripción",
+                            "____________________________________________________"};
+    
+    for (auto& m: menu) {
+        cout << m << endl;
+    };
+        
+    
+    return 0;
+}
 
