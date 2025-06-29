c15be4bd036c7759b408a7e62f9e645f6450b89d
Kendy-16 <kelly.ramos.1652@gmail.com>
2025-06-29 18:15:49 -0500

    Update Proyecto_de_Fase III.cpp

--- Proyecto_de_Fase III.cpp
+++ Proyecto_de_Fase III.cpp
@@ -130,7 +130,7 @@ public:
 class UsuarioTrabajador : public Usuario {
 protected:
     string nombre, celular;
-    int dni;
+    string dni;
     double sueldoBruto;
     int numHijos;
     AFP* afp;
@@ -138,7 +138,7 @@ protected:
     static int asignacionFamiliar;
 
 public:
-    UsuarioTrabajador(string nom, int d, string cel, double sueldo, int hijos, AFP* a, Empresa* emp, string id, string pass)
+    UsuarioTrabajador(string nom, string d, string cel, double sueldo, int hijos, AFP* a, Empresa* emp, string id, string pass)
         : Usuario(id, pass), nombre(nom), dni(d), celular(cel), sueldoBruto(sueldo), numHijos(hijos), afp(a), empresa(emp) {}
     
     
